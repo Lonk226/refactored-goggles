7aea5c87a0f5edb237a1247f4434efc64b3cab6c
Lincoln Jones <lincolnjones226@gmail.com>
2025-05-21 10:46:02 -0400

    h

--- mega-man-2/Scripts/enemy.gd
+++ mega-man-2/Scripts/enemy.gd
@@ -2,11 +2,6 @@ extends Node2D
 
 @export var stats: Resource
 
-func _on_hurtbox_area_entered(area: Area2D) -> void:
-	if area.is_in_group("Lemons"):
-		area.queue_free()
-		hit()
-
 func hit():
 	stats.hp -= 1
 	$"Hit Sound".playing = true
@@ -20,3 +15,8 @@ func _on_hurtbox_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") and not Global.player_invincible:
 		Global.mega_man_health -= stats.damage
 		Global.damage_player.emit()
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Lemons"):
+		area.queue_free()
+		hit()
