0f2b9d422fa6f6327c70b23137075c76ce44d39f
Lincoln Jones <lincolnjones226@gmail.com>
2025-05-23 07:41:19 -0400

    telly

--- mega-man-2/Scenes/Enemies/telly.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var speed = 100
-
-func _on_telly_death() -> void:
-	$Normal.visible = false
-
-func _physics_process(delta: float) -> void:
-	pass
---
+++ mega-man-2/Scripts/telly.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var speed = 80
+
+func _on_telly_death() -> void:
+	$Normal.visible = false
+
+func _physics_process(delta: float) -> void:
+	var player = get_tree().get_first_node_in_group("Player")
+	global_position += (player.global_position - global_position)/ speed
